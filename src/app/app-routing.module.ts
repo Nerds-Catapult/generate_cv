203a77f60b4feef43e671e23e7bce844fc5dfdba
Domains18 <gibsonsgibson88@gmail.com>
2023-03-08 18:12:54 +0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateRegistrationComponent } from './create-registration/create-registration.component';
+import { RegistrationListComponent } from './registration-list/registration-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'register', component: CreateRegistrationComponent},
+  { path: '', redirectTo: 'register', pathMatch: 'full' },
+  {path: 'list', component: RegistrationListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
