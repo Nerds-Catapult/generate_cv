e2a09e2d55648ddb8cfd5168b014d40d5761ea2c
Domains18 <gibsonsgibson88@gmail.com>
2023-03-12 20:49:36 +0300

    featured

--- client/src/components/card/Card.js
+++ client/src/components/card/Card.js
@@ -1,11 +1,24 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 
 
 import './card.scss'
-const Card = () => {
+const Card = ({ item }) => {
   return (
-    <div className='card'>Card</div>
+    <Link to={`product/item/${item.id}`} className='link'>
+      <div className='card'>
+        <div className="image">
+          <img src={item.img} className='mainImg' />
+          <img src={item.img2} 
+          className='secondImg'/>
+        </div>
+        <h2>{item.title}</h2>
+        <div className="prices">
+          <h3> ${item.oldPrice} </h3>
+          <h3> ${item.newPrice} </h3>
+        </div>
+      </div>
+    </Link>
   )
 }
 
--- client/src/components/featuredProducts/featured.js
+++ client/src/components/featuredProducts/featured.js
@@ -1,52 +1,52 @@
 import React from 'react'
-import './featured.sass'
-import Card from '../card/Card';
 
-const data = [
-    {
-        id: 1,
-        img: "https://images.pexels.com/photos/3183150/pexels-photo-3183150.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-        img2: "https://images.pexels.com/photos/3183150/pexels-photo-3183150.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-        isNew: true,
-        oldPrice: 100,
-        newPrice: 50,
-    },
-    {
-        id: 2,
-        img: "https://images.pexels.com/photos/3183150/pexels-photo-3183150.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-        img2: "https://images.pexels.com/photos/3183150/pexels-photo-3183150.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-        isNew: true,
-        oldPrice: 100,
-        newPrice: 50,
-    },
-    {
-        id: 3,
-        img: "https://images.pexels.com/photos/3183150/pexels-photo-3183150.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-        img2: "https://images.pexels.com/photos/3183150/pexels-photo-3183150.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-        isNew: true,
-        oldPrice: 100,
-        newPrice: 50,
-    }
-];
+const featured = ({ type }) => {
 
-const Featured = ({type}) => {
-  return (
-      <div className='featuredProducts'>
-          <div className="top">
-              <h1>{type} products</h1>
-              <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nostrum eius cupiditate, excepturi molestias soluta. Nemo totam, unde temporibus fugit soluta, minima neque debitis dolorem provident inventore tempore, eaque sint.
-              </p>
-          </div>
-          <div className="bottom">
-              {
-                  data.map(item => (
-                      <Card item={item} key={item.id} />
-                  ))
-               }
+    const data = [
+        {
+            id: 1,
+            img: "https://images.pexels.com/photos/1972115/pexels-photo-1972114.jpeg?auto=compress&cs=tinysrgb&w=1600",
+            img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+            title: "Nike Air Max 270",
+            isNew: true,
+            oldPrice: 200,
+            newPrice: 150,
+        },
+        {
+            id: 2,
+            img: "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress&cs=tinysrgb&w=1600",
+            title: "Nike Air Max 270",
+            isNew: true,
+            oldPrice: 200,
+            newPrice: 150,
+        },
+        {
+            id: 3,
+            img: "https://images.pexels.com/photos/1457983/pexels-photo-1457983.jpeg?auto=compress&cs=tinysrgb&w=1600",
+            title: "Nike Air Max 270",
+            isNew: true,
+            oldPrice: 200,
+            newPrice: 150,
+        },
+        {
+            id: 4,
+            img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+            title: "Nike Air Max 270",
+            isNew: true,
+            oldPrice: 200,
+            newPrice: 150,
+        }
+
+    ];
+
+    return (
+        <div className='featuredProducts'>
+            <div className="top">
+                <h1>{type} products</h1>
             </div>
-    </div>
-  )
+            <div className="bottom"></div>
+        </div>
+    )
 }
 
-export default Featured
+export default featured
