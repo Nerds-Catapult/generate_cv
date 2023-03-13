7f3779747cbc69ffb87c2b537d868759f7cb6a49
Domains18 <gibsonsgibson88@gmail.com>
2023-03-13 17:22:13 +0300

    categories

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Slider from '../../components/slider/slider'
 import './home.scss'
 import Featured from '../../components/featuredProducts/featured'
+import { Categories } from '../../components/categories/categories'
 
 function Home() {
   return (
     <div className='home'>
       <Slider />
       <Featured type="featured" />
+      <Categories/>
       <Featured type="trending"/>
     </div>
   )
