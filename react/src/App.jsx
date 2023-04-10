c0fe285c44bda96d0fba43249817b49d4006d7ac
Domains18 <gibsonsgibson88@gmail.com>
2023-04-10 11:38:03 +0300

    navbar
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Navbar from './Navbar/Navbar'
+
+
+
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Navbar />
+    </>
   )
 }
 
---
+++ react/src/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Navbar.scss';
+import { MdRemoveShoppingCart } from 'react-icons/md';
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <nav>
+                <div className="logo">
+                    <h1>Shopka</h1>
+                </div>
+                <div className="affiliate">
+                    <a href="#">Sell On Shopka</a>
+                    <a href="#">Register</a>
+                </div>
+                <div className="search">
+                    <input type="text" placeholder="Search" />
+                    <button>Search</button>
+                </div>
+                <div className="cart">
+                    <MdRemoveShoppingCart />
+                    <span>2</span>
+                </div>
+                <div className="user">
+                    <a href="#">Login</a>
+                    <a href="#">Register</a>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
