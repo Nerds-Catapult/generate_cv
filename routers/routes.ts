7a45887a93b6fde604975f4cfcfa18d3f187f78a
Domains18 <gibsonsgibson88@gmail.com>
2023-05-26 12:32:08 +0300

    unresolved user
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- routers/routes.ts
+++ routers/routes.ts
@@ -1,6 +1,24 @@
 import express, {Request, Response} from 'express'
-
-
-
-
+import { login, register, createTodo } from '../controller/controller';
 const userRouter = express();
+
+
+import { isAuth } from '../middleware/isAuth';
+import todoModel from '../model/Todo';
+import User from '../model/User';
+
+
+userRouter.get("/", async (req: Request, res: Response) => {
+    const user = await User.aggregate([{ $lookup: { from: "todos", localField: "_id", foreignField: "userId", as: "todos" } }, { $unwind: { path: "$todos", preserveNullAndEmptyArrays: true } }])
+    res.send(user)
+});
+
+userRouter.get("/", async (req: Request, res: Response) => {
+    const user = await User.aggregate([{ $lookup: { from: "todos", localField: "_id", foreignField: "userId", as: "todos" } }, { $unwind: { path: "$todos", preserveNullAndEmptyArrays: true } }])
+    res.send(user)
+});
+
+userRouter.get("/addTodo", async (req: Request, res: Response) => {
+    const userData = req.session.user;
+    res.send({ isVerify: true, message: "Add Todo", userData })
+});
