423735e0dd99240e54d3b825c238299fec7019da
Domains18 <gibsonsgibson88@gmail.com>
2023-05-26 12:46:31 +0300

    unresolved user
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- index.ts
+++ index.ts
@@ -4,9 +4,21 @@ const app = Express();
 const port = process.env.PORT || 5000;
 
 import connectDB  from './config/db'
+import bodyParser from 'body-parser';
+import session from 'express-session';
 connectDB()
 
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(session({
+    resave: false,
+    secret: "somesessionsecrets",
+    saveUninitialized: true
+}));
 
+import  userRouter  from './routers/routes';
+app.use('/api', userRouter);
 
 
 
