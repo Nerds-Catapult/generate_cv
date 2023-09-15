6cbc1caff4c5f61de42871322776a9d549eedbb3
Domains18 <gibsonsgibson88@gmail.com>
2023-09-15 16:14:47 +0300

    packages
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import './App.css'
+import React from 'react'
 
 
-const App = () => {
+export default function App() {
   return (
-    <div>App</div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
-
-export default App
