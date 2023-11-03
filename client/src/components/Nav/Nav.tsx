7a2028503f086a65ee682e8ab9258d2629e78276
shadmeoli <shadrack@shopzetu.com>
2023-11-03 19:38:41 +0300

    feat: nav update

--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -1,25 +1,58 @@
+import React, { useState, useMemo } from "react";
 
-
-
-import './Nav.scss'
 const Nav = () => {
-    return (
-        <div className="nav">
-            <h1>Logo</h1>
-            <ul>
-                <li>Resumes</li>
-                <li>CV</li>
-                <li>Cover Letters</li>
-                <li>Career Blog</li>
-                <li>FAQ</li>
-            </ul>
-            <div className="buttons">
-                <button>Sign In</button>
-                <button>Biuld My Resume</button>
-            </div>
-            <hr/>
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-row items-center justify-between pt-4 px-4 backdrop-blur bg-opacity-40 w-screen">
+      <a href="#"></a>
+      <img
+        className="shadow-xl h-16 w-16 rounded-lg items-center justify-center"
+        src="../../assets/logo1.png"
+      />
+      <div className="flex flex-row space-x-6 w-[60%] items-center justify-center">
+        <a
+          className="text-sm hover:text-blue-500 active:text-purple-500"
+          href="#"
+        >
+          Resumes
+        </a>
+        <a
+          className="text-sm hover:text-blue-500 active:text-purple-500"
+          href="#"
+        >
+          CV
+        </a>
+        <a
+          className="text-sm hover:text-blue-500 active:text-purple-500"
+          href="#"
+        >
+          Cover Letter
+        </a>
+        <a
+          className="text-sm hover:text-blue-500 active:text-purple-500"
+          href="#"
+        >
+          Career
+        </a>
+        <a
+          className="text-sm hover:text-blue-500 active:text-purple-500"
+          href="#"
+        >
+          Blog
+        </a>
+        <a
+          className="text-sm hover:text-blue-500 active:text-purple-500"
+          href="#"
+        >
+          FAQs
+        </a>
+      </div>
+      <div className="flex flex-row space-x-4">
+        <button>Sign In</button>
+        <button>Biuld My Resume</button>
+      </div>
+      <hr />
+    </div>
+  );
+};
 
-export default Nav
+export default Nav;
