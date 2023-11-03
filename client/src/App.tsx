a1c1e21e5d26ac409aec5dac3493082235bd256d
Domains18 <gibsonsgibson88@gmail.com>
2023-11-03 23:29:04 +0300

    authentication slice

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css'
-import Nav from './components/Nav/Nav'
-
+import Nav from './components/Nav/Nav';
+import SignIn from './components/auth/SignIn';
+import Login from './components/auth/Login';
+import { Route, Routes } from 'react-router-dom';
 
 export default function App() {
   return (
     <div>
-        <Nav/>
+      <Nav />
+      <Routes>
+          <Route path="signin" element={<SignIn />} />
+          <Route path="login" element={<Login />} />
+        </Routes>
     </div>
   )
 }
--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import authSlice from "../reducers/authSlice";
 
 export const Store = configureStore({
     reducer: {
-        // reducers
+        auth: authSlice,
     }
 });
-
-
