95a955c7acd8cccd7dd90ff05eb7852bb6ae7f40
shadmeoli <shadrack@shopzetu.com>
2023-11-25 22:55:54 +0300

    feat: route additon for page handling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,24 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import Nav from "./components/Nav/Nav";
 import Signup from "./pages/auth/SignUp";
 import Login from "./pages/auth/Login";
 import Landingpage from "./pages/LandingPage/index";
-import { Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
     <>
       <div className="bg-gradient-to-b from-pink-50 to-cyan-50 bg-[url]">
         <Nav />
-        <Landingpage />
+        <BrowserRouter>
+          <Switch>
+            <Route Path="/">
+              <Landingpage />
+            </Route>
+          </Switch>
+        </BrowserRouter>
       </div>
     </>
   );
