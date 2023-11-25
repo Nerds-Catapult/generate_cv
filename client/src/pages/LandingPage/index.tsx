474699a8bdcee073b9cdab36634f2835bc531e32
shadmeoli <shadrack@shopzetu.com>
2023-11-25 17:46:14 +0300

    feat: lading page update

--- client/src/pages/LandingPage/index.tsx
+++ client/src/pages/LandingPage/index.tsx
@@ -1,6 +1,20 @@
 import React from "react";
 
 const Landingpage = () => {
+  const [showForm, setShowForm] = React.useState<boolean>(false);
+
+  const signupForm = () => {
+    if (showForm) {
+          return (
+            <>
+              <View className="h-screen w-screen absolute flex items-center justify-center z-10 backdrop-blur"></View>
+            </>
+          );
+        } else {
+        return
+        }
+  };
+
   return (
     <>
       <div className="w-screen h-screen flex flex-col items-center">
@@ -14,20 +28,21 @@ const Landingpage = () => {
           Elevate Your Career Journey with Confidence!
         </p>
         <div className="w-screen flex justify-center items-center mt-10 space-x-2">
-          <a href="signup" className="cursor-pointer hover:bg-pink-400 rounded bg-pink-500 p-2 text-white font-bold text-sm h-10" >
+          <button
+            onPress={() => setShowForm(true)}
+            className="cursor-pointer hover:bg-pink-400 rounded bg-pink-500 p-2 text-white font-bold text-sm h-10"
+          >
             Get started
-          </a>
-          <button className="hover:border-cyan-800 active:bg-pink-500 active:text-white rounded border-2 border-cyan-300 p-2 text-cyan-500 font-bold text-sm h-10" >
+          </button>
+          <button className="hover:border-cyan-800 active:bg-pink-500 active:text-white rounded border-2 border-cyan-300 p-2 text-cyan-500 font-bold text-sm h-10">
             View plans
           </button>
-        </div >
+        </div>
 
         {/* place where where we will add our testimonials */}
         {/* <h1 className="text-black font-bold mt-20">Our trusted clients</h1>
         <div className="xs:w-[80%] w-screen h-auto md:h-[20%] mt-4 bg-gray-100 transition duration-500 ease-in-out transform">
         </div> */}
-
-
       </div>
     </>
   );
