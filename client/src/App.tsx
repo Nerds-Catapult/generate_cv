3103081c190b25ca82e14fcd35175e91ae68b66e
Domains18 <gibsonsgibson88@gmail.com>
2023-12-01 09:35:42 +0300

    Update dependencies and add user model

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -33,7 +33,7 @@ const App = () => {
         { showNav && (
           <Nav />
         )}
-            <Landingpage />
+            <Signup/>
       </div>
   );
 }
--- server/models/user.go
+++ server/models/user.go
@@ -1,1 +1,10 @@
 package models
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Name string `json:"name"`
+	Email string `json:"email"`
+	Password string `json:"password"`
+}
