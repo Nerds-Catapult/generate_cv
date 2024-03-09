9bc6012b5ea4f6f0c273fbd630daeb443a4c73b8
Domains18 <gibsonsgibson88@gmail.com>
2024-03-09 23:44:40 +0300

    added utils

--- pkg/utils/altcv.go
+++ pkg/utils/altcv.go
@@ -1,1 +1,68 @@
 package utils
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func AltaCvMod() error {
+	StringArray := []string{"pdfx", "biber", "bibhang", "biblabelsep", "pubtype", "bibsetup", "bibitemsep", "trimclip"}
+	file, err := os.OpenFile("altacv.cls", os.O_RDWR, 0644)
+	if err != nil {
+		log.Println("error opening file", err)
+		return err
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(file)
+	scanner := bufio.NewScanner(file)
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(file)
+
+	for _, line := range lines {
+		keepLine := true
+		for _, val := range StringArray {
+			if strings.Contains(line, val) {
+				keepLine = false
+				break
+			}
+		}
+		if keepLine {
+			newLine := strings.Replace(line, "pdfstringdef", "escape", -1)
+			fmt.Fprintf(file, newLine)
+		}
+	}
+	modifications := `
+	% Modification
+	\newcommand{\cvproject}[3]{%
+  	{\large\color{emphasis}#1\par}
+  	\smallskip\normalsize
+  	\ifstrequal{#2}{}{}{
+  	\textbf{\color{accent}#2}\par
+  	\smallskip}
+  	\ifstrequal{#3}{}{}{{\small\makebox[0.5\linewidth][l]{\faCalendar~#3}}}%
+  	\medskip\normalsize
+	}
+	\newcommand{\cvskillstr}[2]{%
+  	\textcolor{emphasis}{\textbf{#1}}\hfill
+  	\textbf{\color{body}#2}\par
+	}
+	`
+	fmt.Fprintln(file, modifications)
+	log.Println("Refactor complete")
+	return nil
+}
---
+++ pkg/utils/utils.go
@@ -0,0 +1,41 @@
+package utils
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+func JsonInput(fname string) []byte {
+	jsonFile, err := os.Open(fname)
+	if err != nil {
+		log.Println(err)
+	}
+	byteValue, _ := io.ReadAll(jsonFile) //ioutil.ReadAll is deprecated
+	return byteValue
+}
+
+func RemoveFiles(fileName string){
+	e := os.RemoveAll(filepath.Dir(fileName))
+	if e != nil {
+		panic(e)
+	}
+}
+
+func RunCommand(command string, stdout, stderr *bytes.Buffer, args ...string) error{
+	cmd := exec.Command(command, args...)
+	if stdout != nil {
+		cmd.Stdout = stdout
+	} else {
+		cmd.Stdout = os.Stdout
+	}
+	if stderr != nil {
+		cmd.Stderr = stderr
+	} else {
+		cmd.Stderr = os.Stderr
+	}
+	return cmd.Run()
+}
